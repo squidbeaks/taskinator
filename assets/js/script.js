55be248a0bd5eaefc5aac38a89214a4cafd10d7a
Leah Russell <leahsigridrussell@gmail.com>
2021-03-01 19:51:18 -0800

    add persistence

--- assets/js/script.js
+++ assets/js/script.js
@@ -221,6 +221,23 @@ var saveTasks = function() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+var loadTasks = function() {
+    var savedTasks = localStorage.getItem("tasks");
+
+    if (savedTasks === null) {
+        return false;
+    }
+    savedTasks = JSON.parse(savedTasks);
+
+    // loop through savedTasks array
+    for (var i = 0; i < savedTasks.length; i++) {
+        // pass each task object into the `createTaskEl()` function
+        createTaskEl(savedTasks[i]);
+    }
+};
+
 pageContentEl.addEventListener("click", taskButtonHandler);
 
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
+
+loadTasks();
